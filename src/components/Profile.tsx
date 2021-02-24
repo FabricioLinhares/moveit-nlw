05929eafdbf85e4193cfd08df451ecbaa9b310d4
Fabricio Linhares Rodrigues <fabriciorodrigues1231@outlook.com>
2021-02-24 09:25:57 -0300

    usar contexto nos componentes

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css'
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/FabricioLinhares.png" alt="Fabricio Linhares"/>
@@ -8,7 +12,7 @@ export function Profile() {
         <strong>Fabricio Linhares</strong>
         <p>
           <img src="icons/level.svg" alt="Level"></img>
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
